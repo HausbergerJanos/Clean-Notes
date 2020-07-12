3d0d694bbf27fcc880d21cd3ea5b669b63a4946e
Janos Hausberger <hausberger.janos@codeyard.eu>
2020-07-12 12:57:09 +0200

    Firestore local testing

--- app/src/androidTest/java/com/codingwithmitch/cleannotes/di/TestAppComponent.kt
+++ app/src/androidTest/java/com/codingwithmitch/cleannotes/di/TestAppComponent.kt
@@ -1,6 +1,6 @@
 package com.codingwithmitch.cleannotes.di
 
-import com.codingwithmitch.cleannotes.business.TempTest
+import com.codingwithmitch.cleannotes.framework.datasource.network.NoteFirestoreServiceTests
 import com.codingwithmitch.cleannotes.framework.presentation.TestBaseApplication
 import dagger.BindsInstance
 import dagger.Component
@@ -21,5 +21,5 @@ interface TestAppComponent : AppComponent {
         fun create(@BindsInstance app: TestBaseApplication): TestAppComponent
     }
 
-    fun inject(tempTest: TempTest)
+    fun inject(noteFirestoreServiceTests: NoteFirestoreServiceTests)
 }
