e9cd482f7f2d2235fe5f20a964e143afefd9ece2
Janos Hausberger <hausberger.janos@codeyard.eu>
2020-07-22 00:48:13 +0200

    NoteListFragment

--- app/src/main/java/com/codingwithmitch/cleannotes/framework/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/codingwithmitch/cleannotes/framework/presentation/splash/SplashFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.codingwithmitch.cleannotes.R
+import com.codingwithmitch.cleannotes.framework.presentation.BaseApplication
 import com.codingwithmitch.cleannotes.framework.presentation.common.BaseNoteFragment
 import com.codingwithmitch.cleannotes.util.cLog
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
+@FlowPreview
+@ExperimentalCoroutinesApi
 class SplashFragment
 constructor(
     private val viewModelFactory: ViewModelProvider.Factory
-) : BaseNoteFragment(R.layout.fragment_splash) {
+): BaseNoteFragment(R.layout.fragment_splash) {
 
     val viewModel: SplashViewModel by viewModels {
         viewModelFactory
@@ -20,10 +26,17 @@ constructor(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navNoteListFragment()
+    }
+
+    private fun navNoteListFragment(){
+        findNavController().navigate(R.id.action_splashFragment_to_noteListFragment)
     }
 
     override fun inject() {
-
+        activity?.run {
+            (application as BaseApplication).appComponent
+        }?: throw Exception("AppComponent is null.")
     }
 }
 
